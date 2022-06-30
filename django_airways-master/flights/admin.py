651e5d6516a2397d8ddb040f515a643facb29219
srikar5737 <71379125+srikar5737@users.noreply.github.com>
2022-06-30 15:12:06 +0530

    sdad

--- django_airways-master/flights/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-from .models import Flight
-
-@admin.register(Flight)
-class FlightAdmin(admin.ModelAdmin):
-	list_display = ('origin', 'destination', 'duration', 'status')
-	search_fields = ('origin', 'destination', 'duration', 'status')
